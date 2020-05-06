420aae2b470d1bd5b05a855250e9e6a83885ef2b
Gocir <ridwanwanwan72@gmail.com>
2020-05-07 00:17:56 +0700

    Make UI termsOfService.dart

---
+++ lib/screens/termsOfService.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TermsOfService extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Ketentuan Layanan'),
+      ),
+      body: Center(
+        child:  Text('Hello Cuk'),
+      ),
+    );
+  }
+}
