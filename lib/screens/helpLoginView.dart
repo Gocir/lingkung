f1afffc385e1da0973f9721cdf984230812eb884
Gocir <ridwanwanwan72@gmail.com>
2020-05-07 00:22:53 +0700

    Make UI helpLoginView.dart

---
+++ lib/screens/helpLoginView.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HelpLoginView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Masuk',
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            color: Colors.black
+          ),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.black
+        ),
+      ),
+      body: Center(
+        child:  Text('Hello Cuk'),
+      ),
+    );
+  }
+}
