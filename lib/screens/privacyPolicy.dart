a33daea37068445eda0904236ea0f653c4f14f16
Gocir <ridwanwanwan72@gmail.com>
2020-05-07 00:17:19 +0700

    Make UI privacyPolicy.dart

---
+++ lib/screens/privacyPolicy.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PrivacyPolicy extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Kebijakan Privasi'),
+      ),
+      body: Center(
+        child:  Text('Hello Cuk'),
+      ),
+    );
+  }
+}
