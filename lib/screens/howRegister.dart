236059b08d84fe5445c2d7762cee1a066511dab5
Gocir <ridwanwanwan72@gmail.com>
2020-05-09 22:13:46 +0700

    Make new File for Help User to Register Account

---
+++ lib/screens/howRegister.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HowToRegister extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Cara membuat akun Linkung'),
+      ),
+      body: Center(
+        child:  Text('Hello Cuk'),
+      ),
+    );
+  }
+}
