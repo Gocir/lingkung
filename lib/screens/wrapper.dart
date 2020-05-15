9e7125d79a4b39121e2043bbd7e9b131db31dc8e
Gocir <ridwanwanwan72@gmail.com>
2020-05-15 18:56:21 +0700

    Make wrapper.dart file

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:lingkung/models/user.dart';
+import 'package:lingkung/screens/authenticate/authenticate.dart';
+import 'package:lingkung/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    final user = Provider.of<User>(context);
+    print(user);
+
+    //return either home or authenticate widget
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return HomePage();
+    }
+  }
+}
