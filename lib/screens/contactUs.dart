c750b012a7ad1994666aa2d0d2691bd2234bb5f9
Gocir <ridwanwanwan72@gmail.com>
2020-05-22 23:42:34 +0700

    Just Make a File ContactUs

---
+++ lib/screens/contactUs.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ContactUs extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Cara masuk ke akun Linkung'),
+      ),
+      body: Center(
+        child:  Text('Hello Cuk'),
+      ),
+    );
+  }
+}
