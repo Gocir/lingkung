78a5648e83e3d58ad7d9aa8ad003bba033367337
Gocir <ridwanwanwan72@gmail.com>
2020-06-22 14:39:58 +0700

    fix code to display in trashBankDetail.dart

--- lib/widgets/trashReceiveLisTile.dart
+++ lib/widgets/trashReceiveLisTile.dart
@@ -12,34 +12,37 @@ class TrashReceiveLisTile extends StatelessWidget {
     final partner = Provider.of<PartnerProvider>(context);
 
     final trashReceiveProvider = Provider.of<TrashReceiveProvider>(context);
-    trashReceiveProvider.loadTrashReceiveByPartner(partner.businessPartnerModel.id);
+    trashReceiveProvider.loadTrashReceiveByPartner("HBm0o5t7llQFQmyIeylNPnLhzyU2");
     
-    return ListView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: trashReceiveProvider.trashReceiveByPartner.length,
-        itemBuilder: (_, index) {
-          return Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)),
-            child: ListTile(
-              title: CustomText(
-                text: trashReceiveProvider.trashReceiveByPartner[index].trashName,
-                size: 16,
-                color: grey,
-                weight: FontWeight.w500,
+    return Container(
+      height: 200,
+      child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: trashReceiveProvider.trashReceiveByPartner.length,
+          itemBuilder: (_, index) {
+            return Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: ListTile(
+                title: CustomText(
+                  text: trashReceiveProvider.trashReceiveByPartner[index].trashName,
+                  size: 16,
+                  color: grey,
+                  weight: FontWeight.w500,
+                ),
+                subtitle: CustomText(
+                  text: 'Rp ${trashReceiveProvider.trashReceiveByPartner[index].price.toString()} /Kg',
+                  size: 16,
+                  color: black,
+                  weight: FontWeight.w600,
+                ),
+                trailing: GestureDetector(
+                  child: Icon(Icons.edit, color: blue,),
+                  onTap: () {},
+                ),
               ),
-              subtitle: CustomText(
-                text: 'Rp ${trashReceiveProvider.trashReceiveByPartner[index].price.toString()} /Kg',
-                size: 16,
-                color: black,
-                weight: FontWeight.w600,
-              ),
-              trailing: GestureDetector(
-                child: Icon(Icons.edit, color: blue,),
-                onTap: () {},
-              ),
-            ),
-          );
-        });
+            );
+          }),
+    );
   }
 }
