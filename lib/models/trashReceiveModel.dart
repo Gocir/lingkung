82471e684f7508db771b4730e401bb83044b0a52
Gocir <ridwanwanwan72@gmail.com>
2020-06-25 14:57:09 +0700

    Modified variabel type price

--- lib/models/trashReceiveModel.dart
+++ lib/models/trashReceiveModel.dart
@@ -2,27 +2,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TrashReceiveModel{
   static const ID = "id";
-  static const PRICE = "price";
   static const TRASH_NAME = "trashName";
+  static const IMAGE = "image";
+  static const PRICE = "price";
   static const PARTNER_ID = "partnerId";
 
   String _id;
-  double _price;
   String _trashName;
+  int _price;
+  String _image;
   String _partnerId;
 
 //  getters
   String get id => _id;
-  double get price => _price;
   String get trashName => _trashName;
+  int get price => _price;
+  String get image => _image;
   String get partnerId => _partnerId;
-
-// public variable
 
   TrashReceiveModel.fromSnapshot(DocumentSnapshot snapshot){
     _id = snapshot.data[ID];
-    _price = snapshot.data[PRICE];
     _trashName = snapshot.data[TRASH_NAME];
+    _price = snapshot.data[PRICE];
+    _image = snapshot.data[IMAGE];
     _partnerId = snapshot.data[PARTNER_ID];
   }
 
