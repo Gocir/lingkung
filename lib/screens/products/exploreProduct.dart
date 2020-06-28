16018acb2049c60f4b7bf5c922152f19b9676834
Gocir <ridwanwanwan72@gmail.com>
2020-06-29 01:24:53 +0700

    Add code for display product

--- lib/screens/products/exploreProduct.dart
+++ lib/screens/products/exploreProduct.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lingkung/providers/productProvider.dart';
+import 'package:lingkung/utilities/colorStyle.dart';
+import 'package:lingkung/utilities/textStyle.dart';
+import 'package:provider/provider.dart';
 
 class ExploreProductPage extends StatefulWidget {
   @override
@@ -8,55 +12,68 @@ class ExploreProductPage extends StatefulWidget {
 class _ExploreProductPageState extends State<ExploreProductPage> {
   @override
   Widget build(BuildContext context) {
+    final productProvider = Provider.of<ProductProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xff5bc0eb),
-        title: Text(
-          'Produk',
-          style: TextStyle(
-            fontFamily: "Poppins",
-          ),
+        backgroundColor: blue,
+        title: CustomText(
+          text: 'Produk Ramah Lingkung',
+          size: 18.0,
+          color: white,
         ),
         actions: <Widget>[
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Icon(
               Icons.shopping_cart,
-              color: const Color(0xffffffff),
+              color: white,
             ),
           ),
         ],
       ),
-      backgroundColor: Colors.white,
-      body: ListView(
-        padding: EdgeInsets.only(left: 20.0, right: 20.0),
-        children: <Widget>[
-          Wrap(
-            direction: Axis.vertical,
-            alignment: WrapAlignment.start,
-            spacing: 10.0,
-            runSpacing: 20.0,
-            children: <Widget>[
-              
-            ],
-          )
-        ],
-      ),
+      backgroundColor: white,
+      body: GridView.builder(
+              itemCount: productProvider.products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, childAspectRatio: 2 / 3),
+              itemBuilder: (_, index) {
+                return Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.width / 2.4,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10.0),
+                                  topRight: Radius.circular(10.0),
+                                ),
+                                child: Image.network(
+                                    '${productProvider.products[index].image.toString()}',
+                                    fit: BoxFit.cover))),
+                        Container(
+                          height: 50.0,
+                          padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                          child: CustomText(
+                              text:
+                                  '${productProvider.products[index].name} bifzouzp'),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                          child: CustomText(
+                            text:
+                                'Rp ${productProvider.products[index].price.toString()}',
+                            color: yellow,
+                            weight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ));
+              }),
     );
   }
 }
-//   Widget _buildFoodItems(Food food) {
-//     return Container(
-//       margin: EdgeInsets.only(bottom: 20.0),
-//       child: BoughtFood(
-//         id: food.id,
-//         name: food.name,
-//         imagePath: food.imagePath,
-//         category: food.category,
-//         discount: food.discount,
-//         price: food.price,
-//         ratings: food.ratings,
-//       ),
-//     );
-//   }
-// }
--- lib/screens/products/myProduct.dart
+++ lib/screens/products/myProduct.dart
@@ -3,25 +3,83 @@ import 'package:lingkung/providers/userProvider.dart';
 import 'package:lingkung/providers/productProvider.dart';
 import 'package:lingkung/utilities/colorStyle.dart';
 import 'package:lingkung/utilities/textStyle.dart';
-import 'package:lingkung/widgets/productLisTile.dart';
 import 'package:provider/provider.dart';
 
-class MyProductPage extends StatelessWidget {
+class MyProductPage extends StatefulWidget {
+  @override
+  _MyProductPageState createState() => _MyProductPageState();
+}
+
+class _MyProductPageState extends State<MyProductPage> {
   @override
   Widget build(BuildContext context) {
     final userProvider = Provider.of<UserProvider>(context);
     final productProvider = Provider.of<ProductProvider>(context);
-    
-    productProvider.loadProductByUser(userProvider.user.uid);
 
+    productProvider.loadProductByUser(userProvider.user.uid);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: blue,
-        title: CustomText(text: 'Produk Toko Saya', size: 20.0, color: white, weight: FontWeight.w600,
+        appBar: AppBar(
+          backgroundColor: blue,
+          title: CustomText(
+            text: 'Produk Toko Saya',
+            size: 20.0,
+            color: white,
+            weight: FontWeight.w600,
+          ),
         ),
-      ),
-      backgroundColor: Colors.white,
-      body: Container(padding: EdgeInsets.all(16.0), child: ProductLisTile())
-    );
+        backgroundColor: Colors.white,
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: GridView.builder(
+              itemCount: productProvider.productByUser.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, childAspectRatio: 2 / 3.4),
+              itemBuilder: (_, index) {
+                return Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.width / 2.4,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10.0),
+                                  topRight: Radius.circular(10.0),
+                                ),
+                                child: Image.network(
+                                    '${productProvider.productByUser[index].image.toString()}',
+                                    fit: BoxFit.cover))),
+                        Container(
+                          height: 50.0,
+                          padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                          child: CustomText(
+                              text:
+                                  '${productProvider.productByUser[index].name} bifzouzp'),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                          child: CustomText(
+                            text:
+                                'Rp ${productProvider.productByUser[index].price.toString()}',
+                            color: yellow,
+                            weight: FontWeight.w500,
+                          ),
+                        ),
+                        Divider(
+                          height: 5.0,
+                        ),
+                        ButtonBar(
+                          children: <Widget>[
+                            Icon(Icons.edit, color: blue),
+                            Icon(Icons.delete_outline, color: yellow)
+                          ],
+                        )
+                      ],
+                    ));
+              }),
+        ));
   }
 }
