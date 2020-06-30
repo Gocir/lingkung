bdda1e0e7268c719e557fbce1e82d8c7e728bfe7
Gocir <ridwanwanwan72@gmail.com>
2020-06-30 23:49:16 +0700

    Modified UI product display

--- lib/screens/products/detailProduct.dart
+++ lib/screens/products/detailProduct.dart
@@ -20,118 +20,117 @@ class _DetailProductState extends State<DetailProduct> {
     // userProvider.reloadUserModel(widget.productModel.userId);
 
     return Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              pinned: true,
-              floating: true,
-              backgroundColor: blue,
-              expandedHeight: 200.0,
-              // leading: Container(
-              //   margin: EdgeInsets.only(left: 16.0, top: 16.0),
-              //   decoration: BoxDecoration(
-              //     color: black.withOpacity(0.2),
-              //     borderRadius: BorderRadius.circular(100.0),
-              //   ),
-              //   child: IconButton(
-              //     icon: Icon(Icons.arrow_back),
-              //     onPressed: () {
-              //       Navigator.pop(context);
-              //     },
-              //   ),
-              // ),
-              // actions: <Widget>[
-              //   Container(
-              //     margin: EdgeInsets.only(right: 16.0, top: 16.0),
-              //     decoration: BoxDecoration(
-              //       color: black.withOpacity(0.2),
-              //       borderRadius: BorderRadius.circular(100.0),
-              //     ),
-              //     child: IconButton(
-              //       icon: Icon(Icons.shopping_cart),
-              //       onPressed: () {
-              //         // Navigator.pushReplacement(
-              //         //     context,
-              //         //     MaterialPageRoute(
-              //         //       builder: (context) => TrashBankList(),
-              //         //     ));
-              //       },
-              //     ),
-              //   )
-              // ],
-              flexibleSpace: FlexibleSpaceBar(
-                // title: CustomText(
-                //   text: 'BS. ${widget.partner.name}',
-                //   size: 20.0,
-                //   weight: FontWeight.w600,
-                // ),
-                background: Image.network(
-                  "${widget.productModel.image}",
-                  fit: BoxFit.cover,
+        appBar: AppBar(
+          backgroundColor: blue,
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: white),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Icon(Icons.shopping_cart, color: white),
+            )
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      opaque: false,
+                        pageBuilder: (BuildContext context, _, __) => GestureDetector(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Container(
+                              width: double.infinity,
+                              height: double.infinity,
+                              color: black.withOpacity(0.9),
+                              child: Center(
+                                child: Hero(
+                                  tag: 'ProductImage',
+                                  child: Image.network(
+                                    '${widget.productModel.image}',
+                                  ),
+                                ),
+                              ),
+                            ))));
+              },
+              child: Hero(
+                tag: 'ProductImage',
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width/1.2,
+                  child: Image.network(
+                    '${widget.productModel.image}',
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-            SliverFillRemaining(
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    CustomText(
-                        text: '${widget.productModel.name}',
-                        size: 16,
-                        weight: FontWeight.w600),
-                    SizedBox(height: 20.0),
-                    CustomText(
-                      text: 'Rp${widget.productModel.price}',
-                      size: 18,
-                    ),
-                    SizedBox(height: 10.0),
-                    // Divider(),
-                    // Card(
-                    //   child: ListTile(
-                    //       leading: Container(
-                    //           height: 60.0,
-                    //           width: 60.0,
-                    //           child: Image.asset(
-                    //             "assets/images/user.png",
-                    //             fit: BoxFit.cover,
-                    //           )),
-                    //       title: CustomText(
-                    //           text: (userProvider.userModel.id == widget.productModel.userId) ? '${userProvider.userModel.name}' : null,
-                    //           weight: FontWeight.w500),
-                    //       dense: true),
-                    // ),
-                    Divider(),
-                    CustomText(
-                        text: 'Informasi Produk',
-                        size: 16.0,
-                        weight: FontWeight.w500),
-                    SizedBox(height: 5.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        CustomText(
-                          text: 'Stok',
-                        ),
-                        CustomText(
-                          text: '${widget.productModel.stock}',
-                        ),
-                      ],
-                    ),
-                    Divider(),
-                    CustomText(
-                        text: 'Deskripsi Produk',
-                        size: 16.0,
-                        weight: FontWeight.w500),
-                    SizedBox(height: 5.0),
-                    CustomText(
-                      text: '${widget.productModel.description}',
-                    ),
-                  ],
-                ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CustomText(
+                      text: '${widget.productModel.name}',
+                      size: 16,
+                      weight: FontWeight.w500),
+                  SizedBox(height: 20.0),
+                  CustomText(
+                    text: 'Rp${widget.productModel.price}',
+                    size: 18,
+                    color: green,
+                    weight: FontWeight.w500
+                  ),
+                  SizedBox(height: 10.0),
+                  // Divider(),
+                  // Card(
+                  //   child: ListTile(
+                  //       leading: Container(
+                  //           height: 60.0,
+                  //           width: 60.0,
+                  //           child: Image.asset(
+                  //             "assets/images/user.png",
+                  //             fit: BoxFit.cover,
+                  //           )),
+                  //       title: CustomText(
+                  //           text: (userProvider.userModel.id == widget.productModel.userId) ? '${userProvider.userModel.name}' : null,
+                  //           weight: FontWeight.w500),
+                  //       dense: true),
+                  // ),
+                  Divider(),
+                  CustomText(
+                      text: 'Informasi Produk',
+                      size: 16.0,
+                      weight: FontWeight.w500),
+                  SizedBox(height: 5.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      CustomText(
+                        text: 'Stok',
+                      ),
+                      CustomText(
+                        text: '${widget.productModel.stock}',
+                        color: yellow,
+                      ),
+                    ],
+                  ),
+                  Divider(),
+                  CustomText(
+                      text: 'Deskripsi Produk',
+                      size: 16.0,
+                      weight: FontWeight.w500),
+                  SizedBox(height: 5.0),
+                  CustomText(
+                    text: '${widget.productModel.description}',
+                  ),
+                ],
               ),
-            )
+            ),
           ],
         ),
         bottomNavigationBar: Container(
@@ -145,13 +144,12 @@ class _DetailProductState extends State<DetailProduct> {
             children: <Widget>[
               Expanded(
                 flex: 1,
-                child: RaisedButton(
+                child: OutlineButton(
                   color: white,
-                  elevation: 1.0,
                   shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      side: BorderSide(color: yellow, width: 2.0)),
-                  child: Icon(Icons.message, color: yellow),
+                      borderRadius: BorderRadius.circular(10)),
+                  borderSide: BorderSide(color: yellow, width: 2.0),
+                  child: Icon(Icons.chat, color: yellow),
                   onPressed: () {
                     // Navigator.push(
                     //     context,
@@ -164,12 +162,11 @@ class _DetailProductState extends State<DetailProduct> {
               SizedBox(width: 10.0),
               Expanded(
                 flex: 2,
-                child: RaisedButton(
+                child: OutlineButton(
                   color: white,
-                  elevation: 1.0,
                   shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      side: BorderSide(color: green, width: 2.0)),
+                      borderRadius: BorderRadius.circular(10)),
+                  borderSide: BorderSide(color: green, width: 2.0),
                   child: CustomText(
                       text: 'Beli', color: green, weight: FontWeight.w700),
                   onPressed: () {
@@ -186,7 +183,7 @@ class _DetailProductState extends State<DetailProduct> {
                 flex: 2,
                 child: RaisedButton(
                   color: green,
-                  elevation: 1.0,
+                  elevation: 0.0,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10)),
                   child: Row(
--- lib/screens/products/exploreProduct.dart
+++ lib/screens/products/exploreProduct.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lingkung/providers/productProvider.dart';
+import 'package:lingkung/screens/products/detailProduct.dart';
 import 'package:lingkung/utilities/colorStyle.dart';
 import 'package:lingkung/utilities/textStyle.dart';
 import 'package:provider/provider.dart';
@@ -43,41 +44,51 @@ class _ExploreProductPageState extends State<ExploreProductPage> {
               return Card(
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10.0)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.width * 0.44,
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10.0),
-                                topRight: Radius.circular(10.0),
-                              ),
-                              child: Image.network(
-                                  '${productProvider.products[index].image.toString()}',
-                                  fit: BoxFit.cover))),
-                      Container(
-                        height: 40.0,
-                        padding: EdgeInsets.only(left: 8.0, right: 8.0),
-                        child: Text(
-                          '${productProvider.products[index].name}',
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          style: TextStyle(
-                              fontSize: 12.0,
-                              fontFamily: "Poppins",
-                              fontWeight: FontWeight.normal),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DetailProduct(
+                                  productModel:
+                                      productProvider.products[index])));
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.width * 0.44,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10.0),
+                                  topRight: Radius.circular(10.0),
+                                ),
+                                child: Image.network(
+                                    '${productProvider.products[index].image.toString()}',
+                                    fit: BoxFit.cover))),
+                        Container(
+                          height: 40.0,
+                          padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                          child: Text(
+                            '${productProvider.products[index].name}',
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            style: TextStyle(
+                                fontSize: 12.0,
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.normal),
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 8.0, right: 8.0),
-                        child: CustomText(
-                          text: 'Rp${productProvider.products[index].price}',
-                          weight: FontWeight.w500,
+                        Padding(
+                          padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                          child: CustomText(
+                            text: 'Rp${productProvider.products[index].price}',
+                            weight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ));
             }),
       ),
