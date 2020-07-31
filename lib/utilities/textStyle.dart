a374c372f1134f864abe7d06b919e3c950e4eb7f
Gocir <ridwanwanwan72@gmail.com>
2020-07-31 17:02:58 +0700

    Add code text style

--- lib/utilities/textStyle.dart
+++ lib/utilities/textStyle.dart
@@ -6,18 +6,22 @@ class CustomText extends StatelessWidget {
   final double size;
   final Color color;
   final FontWeight weight;
+  final FontStyle style;
+  final TextAlign align;
 
   //name constructor that has a positional parameters with the text required and the other parameters optional
-  CustomText({@required this.text, this.size, this.color, this.weight});
+  CustomText({@required this.text, this.size, this.color, this.style, this.align, this.weight});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      textAlign: TextAlign.left,
       style: TextStyle(
+        color: color ?? black,
         fontFamily: "Poppins",
         fontSize: size ?? 14,
-        color: color ?? black,
+        fontStyle: style ?? FontStyle.normal,
         fontWeight: weight ?? FontWeight.normal),
     );
   }
