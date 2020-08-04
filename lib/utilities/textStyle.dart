19adc29be63570aa3f45a886b5975fe2aeafb7ae
Gocir <ridwanwanwan72@gmail.com>
2020-08-04 13:06:16 +0700

    Add widget style for text

--- lib/utilities/textStyle.dart
+++ lib/utilities/textStyle.dart
@@ -3,26 +3,38 @@ import 'package:lingkung/utilities/colorStyle.dart';
 
 class CustomText extends StatelessWidget {
   final String text;
-  final double size;
-  final Color color;
-  final FontWeight weight;
-  final FontStyle style;
   final TextAlign align;
+  final TextOverflow over;
+  final int line;
+  final Color color;
+  final double size;
+  final FontStyle style;
+  final FontWeight weight;
 
   //name constructor that has a positional parameters with the text required and the other parameters optional
-  CustomText({@required this.text, this.size, this.color, this.style, this.align, this.weight});
+  CustomText(
+      {@required this.text,
+      this.align,
+      this.over,
+      this.line,
+      this.color,
+      this.size,
+      this.style,
+      this.weight});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      textAlign: TextAlign.left,
+      textAlign: align ?? TextAlign.left,
+      overflow: over,
+      maxLines: line,
       style: TextStyle(
-        color: color ?? black,
-        fontFamily: "Poppins",
-        fontSize: size ?? 14,
-        fontStyle: style ?? FontStyle.normal,
-        fontWeight: weight ?? FontWeight.normal),
+          color: color ?? black,
+          fontFamily: "Poppins",
+          fontSize: size ?? 14,
+          fontStyle: style,
+          fontWeight: weight),
     );
   }
 }
