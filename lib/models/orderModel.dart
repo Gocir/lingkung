a1997701519cdbff4b2edee9e00ea72703f0cd6d
Gocir <ridwanwanwan72@gmail.com>
2020-09-04 18:42:39 +0700

    Memperbarui snapshot data address, list product, dan shipping

--- lib/models/orderModel.dart
+++ lib/models/orderModel.dart
@@ -1,4 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lingkung/models/addressModel.dart';
+import 'package:lingkung/models/cartItemModel.dart';
+import 'package:lingkung/models/shippingModel.dart';
 
 class OrderModel{
   static const ID = "id";
@@ -8,7 +11,7 @@ class OrderModel{
   static const LIST_PRODUCT = "listProduct";
   static const SHIPPING = "shipping";
   static const SHIPPING_PRICE = "shippingPrice";
-  static const SUB_TOTAL = "subTotal";
+  static const SUB_TOTAL_PRODUCT = "subTotalProduct";
   static const NOTE = "note";
   static const TOTAL = "total";
   static const STATUS = "status";
@@ -16,9 +19,9 @@ class OrderModel{
 
   String _id;
   String _userId;
-  String _storeOwnerId;
+  // String _storeOwnerId;
   int _shippingPrice;
-  int _subTotal;
+  int _subTotalProduct;
   String _note;
   int _total;
   String _status;
@@ -27,9 +30,9 @@ class OrderModel{
 //  getters
   String get id => _id;
   String get userId => _userId;
-  String get storeOwnerId => _storeOwnerId;
+  // String get storeOwnerId => _storeOwnerId;
   int get shippingPrice => _shippingPrice;
-  int get subTotal => _subTotal;
+  int get subTotalProduct => _subTotalProduct;
   String get note => _note;
   int get total => _total;
   String get status => _status;
@@ -37,21 +40,36 @@ class OrderModel{
 
 // public variable
   List listProduct;
-  List address;
-  List shipping;
+  AddressModel address;
+  List storeOwnerId;
+  ShippingModel shipping;
 
   OrderModel.fromSnapshot(DocumentSnapshot snapshot){
     _id = snapshot.data[ID];
     _userId = snapshot.data[USER_ID];
-    _storeOwnerId = snapshot.data[STORE_OWNER_ID];
-    address = snapshot.data[ADDRESS];
-    listProduct = snapshot.data[LIST_PRODUCT];
-    shipping = snapshot.data[SHIPPING];
+    storeOwnerId = snapshot.data[STORE_OWNER_ID];
+    (snapshot.data[ADDRESS] != null)
+        ? address = AddressModel.fromMap(snapshot.data[ADDRESS])
+        : address = snapshot.data[SHIPPING];
+    (snapshot.data[LIST_PRODUCT] != null)
+        ? listProduct = convertListProducts(snapshot.data[LIST_PRODUCT])
+        : listProduct = [];
+    (snapshot.data[SHIPPING] != null)
+        ? shipping = ShippingModel.fromMap(snapshot.data[SHIPPING])
+        : shipping = snapshot.data[SHIPPING];
     _shippingPrice = snapshot.data[SHIPPING_PRICE];
-    _subTotal = snapshot.data[SUB_TOTAL];
+    _subTotalProduct = snapshot.data[SUB_TOTAL_PRODUCT];
     _note = snapshot.data[NOTE];
     _total = snapshot.data[TOTAL];
     _status = snapshot.data[STATUS];
     _createdAt = snapshot.data[CREATED_AT];
+  }
+
+  List<CartItemModel> convertListProducts(List product) {
+    List<CartItemModel> convertedProduct = [];
+    for (Map productItem in product) {
+      convertedProduct.add(CartItemModel.fromMap(productItem));
+    }
+    return convertedProduct;
   }
 }
